5c536690c9deb82e9ac6cf1ba17dbcde82519a77
Andriel Figueredo <andriel@Andriels-MacBook-Pro.local>
2025-12-16 03:38:26 -0300

    chore(docker-compose): update entrypoint to install cron service before starting the app for proper functionality chore(umbrel-app): bump version from 1.0.5 to 1.0.6 and update description to clarify app capabilities chore(exports.sh): clear ngrok authtoken for security reasons

--- andriel-ngrok/exports.sh
+++ andriel-ngrok/exports.sh
@@ -1,2 +1,2 @@
 # Get your authtoken from: https://dashboard.ngrok.com/get-started/your-authtoken
-export APP_NGROK_AUTHTOKEN="2vIWwQ3UPqA92vM2xuepD39Qo0v_5iELacKrvr4QbfRvnqSJF"
+export APP_NGROK_AUTHTOKEN=""
